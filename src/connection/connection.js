981ebb19291937a8d43897091d51583f8fad083b
Saranraj0100 <sraji20055@gmail.com>
2026-04-23 08:02:28 +0530

    schoolInfoSection

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,10 +1,10 @@
 const mysql2=require('mysql2')
 
 const connection=mysql2.createConnection({
-    host:"sql3.freesqldatabase.com",
-    port:"3306",
-    user:"sql3823419",
-    password:"t3WKGlRLDX!",
-    database:"sql3823419",
+    host:"shinkansen.proxy.rlwy.net",
+    port:"19179",
+    user:"root",
+    password:"PTAFqZZBFiOKczhTUKNOzrATujCUCWDI",
+    database:"School_Db",
 })
 module.exports={connection}
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { router } = require('./router/router')
 const dbconnection = require('./connection/connection')
 // const port= 3000
 
-const whitelist = ['http://localhost:5173','http://localhost:5174','http://192.168.1.50:5173','http://192.168.1.50:5174', 'http://192.168.1.50:3001']
+const whitelist = ['http://localhost:5173','http://localhost:5174','http://192.168.1.50:5173','http://192.168.1.50:5174', 'http://192.168.1.50:3001',"http://192.168.31.203:5173"]
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.includes(origin) || !origin) {
